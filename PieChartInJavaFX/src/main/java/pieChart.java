b9a35c629d39804e2d9b5bffceb5032d2d0ce315
sahilkairon <sahilkairon66@gmail.com>
2022-07-09 11:26:14 +0530

    Add files via upload

--- PieChartInJavaFX/src/main/java/pieChart.java
+++ PieChartInJavaFX/src/main/java/pieChart.java
@@ -2,10 +2,15 @@ import com.sun.javafx.application.LauncherImpl;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.image.WritableImage;
 import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.io.File;
 
 public class pieChart extends Application {
     @Override
@@ -27,14 +32,29 @@ public class pieChart extends Application {
 //        animation parameters
 
         Group root = new Group(pChart);
-        Scene scene = new Scene(root,550,450);
+        Scene scene = new Scene(root,1000,1000);
         primaryStage.setTitle("demo Piechart");
         primaryStage.setScene(scene);
         primaryStage.show();
 
+//        store PiChart as image to below location
+
+        saveAsPng(scene,"/Users/sahilkairon/Desktop/images.png");
+
+
+
 
     }
 
+    public void saveAsPng(Scene scene, String path) {
+        WritableImage image = scene.snapshot(null);
+        File file = new File(path);
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         Application.launch(args);
     }
